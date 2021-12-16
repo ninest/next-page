ddf38b8923c4d1acb238dc7b7ae85d2e3332b275
ninest <parth.kabra@gmail.com>
2021-12-17 00:39:02 +0800

    Use custom Google Fonts

---
+++ pages/_document.tsx
@@ -0,0 +1,31 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: any) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            rel="preload"
+            href="/karla.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link rel="stylesheet" href="/font.css" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
